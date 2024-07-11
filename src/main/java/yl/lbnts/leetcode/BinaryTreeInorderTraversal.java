f7203ec254c139897eaeba7aaa00a244fae7d9e4
yelyzavetalubenets <el1111zavetaaaaa@gmail.com>
2024-07-11 20:42:43 +0300

    Add realization of BinaryTreeInorderTraversal

---
+++ src/main/java/yl/lbnts/leetcode/BinaryTreeInorderTraversal.java
@@ -0,0 +1,67 @@
+package yl.lbnts.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given the root of a binary tree, return the inorder traversal of its nodes' values.
+ * <p>
+ * Example 1:
+ * Input: root = [1,null,2,3]
+ * Output: [1,3,2]
+ * <p>
+ * Example 2:
+ * Input: root = []
+ * Output: []
+ * <p>
+ * Example 3:
+ * Input: root = [1]
+ * Output: [1]
+ */
+public class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        TreeNode treeNode3 = new TreeNode(3, null, null);
+        TreeNode treeNode2 = new TreeNode(2, treeNode3, null);
+        TreeNode treeNode1 = new TreeNode(1, null, treeNode2);
+
+        System.out.println(inorderTraversal(treeNode1));
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        inOrder(root, result);
+        return result;
+    }
+
+    private static void inOrder(TreeNode root, ArrayList<Integer> result) {
+        //terminating condition
+        if (root == null) {
+            return;
+        }
+        //recursively call inOrder on the left subtree
+        inOrder(root.left, result);
+        // add the current node's value to the result list
+        result.add(root.val);
+        // recursively call inOrder on the right subtree
+        inOrder(root.right, result);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}

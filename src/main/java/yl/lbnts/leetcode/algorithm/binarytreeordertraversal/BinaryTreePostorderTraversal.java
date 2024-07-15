df421dec1ca8bc5c4180637344e040a05dd97fe9
yelyzavetalubenets <el1111zavetaaaaa@gmail.com>
2024-07-15 20:13:00 +0300

    Add realization of BinaryTreePostorderTraversal and refactor a realization of BinaryTreePreorderTraversal

--- src/main/java/yl/lbnts/leetcode/algorithm/binarytreeordertraversal/BinaryTreePostorderTraversal.java
+++ src/main/java/yl/lbnts/leetcode/algorithm/binarytreeordertraversal/BinaryTreePostorderTraversal.java
@@ -3,27 +3,27 @@ package yl.lbnts.leetcode.algorithm.binarytreeordertraversal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BinaryTreePreorderTraversal {
+public class BinaryTreePostorderTraversal {
     public static void main(String[] args) {
         TreeNode treeNode3 = new TreeNode(3, null, null);
         TreeNode treeNode2 = new TreeNode(2, treeNode3, null);
         TreeNode treeNode1 = new TreeNode(1, null, treeNode2);
-        System.out.println(preorderTraversal(treeNode1));
+        System.out.println(postorderTraversal(treeNode1));
     }
 
-    public static List<Integer> preorderTraversal(TreeNode root) {
+    public static List<Integer> postorderTraversal(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<>();
-        preOrder(root, result);
+        postOrder(root, result);
         return result;
     }
 
-    private static void preOrder(TreeNode root, ArrayList<Integer> result) {
+    private static void postOrder(TreeNode root, ArrayList<Integer> result) {
         if (root == null) {
             return;
         }
 
+        postOrder(root.left, result);
+        postOrder(root.right, result);
         result.add(root.val);
-        preOrder(root.left, result);
-        preOrder(root.right, result);
     }
 }
